c4e33c69daf7f76142ee58a0d7b063ce4c1d9023
fnless <fnless>
2025-03-26 17:49:15 +0800

    add notes
    Signed-off-by: fnless <fnless>

--- examples/fasthttp/trace/tracer.go
+++ examples/fasthttp/trace/tracer.go
@@ -5,5 +5,12 @@ import (
 )
 
 var (
+	// 虽然这里的 tracer 会在 pkg/otel.init() 初始化之前
+	// 但是在 pkg/otel.init() 之后，tracer 会被 DelegateTracerProvider 代理
+	// 详情参见:
+	// - https://github.com/open-telemetry/opentelemetry-go/blob/v1.35.0/internal/global/state.go#L109-L112
+	// - https://github.com/open-telemetry/opentelemetry-go/blob/v1.35.0/internal/global/trace.go#L68-L72
+	// - https://github.com/open-telemetry/opentelemetry-go/blob/v1.35.0/internal/global/trace.go#L137-L139
+	// - https://github.com/open-telemetry/opentelemetry-go/blob/v1.35.0/internal/global/trace.go#L144-L147
 	tracer = otel.Tracer("otel-examples/fasthttp")
 )
